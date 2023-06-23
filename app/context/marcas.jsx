e35724af7aed70e6862980259681e8f8a03bddeb
KPI01 <jorgelurd11@outlook.com>
2023-06-23 18:43:18 +0200

    Creted context of links, productos and marcas

---
+++ app/context/marcas.jsx
@@ -0,0 +1,29 @@
+export const marcas = [
+    {
+        label: "Nestlé Venezuela, S.A"
+    },
+    {
+        label: "Alfonzo Rivas & Cia, C.A."
+    },
+    {
+        label: "PEPSICO Alimentos, S.C.A"
+    },
+    {
+        label: "Envaprimol"
+    },
+    {
+        label: "El Tunal, C.A."
+    },
+    {
+        label: "Plumrose Latinoamricana, C.A."
+    },
+    {
+        label: "Gombi Ind. Alimenticias, C.A."
+    },
+    {
+        label: "Ind. Lacteas Torondoy, C.A."
+    },
+    {
+        label: "General de Alimentos Nisa, C.A."
+    },
+]
---
+++ app/context/pages.jsx
@@ -0,0 +1,18 @@
+export const links = [
+  {
+    label: "Inicio",
+    route: "/main"
+  },
+  {
+    label: "Ofertas",
+    route: "/ofertas",
+  },
+  {
+    label: "Productos",
+    route: "/productos",
+  },
+  {
+    label: "Marcas",
+    route: "/marcas",
+  },
+];
