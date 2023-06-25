7981efb7b120ec1ecdf8dade695cc42f1378bfa6
KPI01 <jorgelurd11@outlook.com>
2023-06-25 22:10:58 +0200

    Addes login and cart page

---
+++ app/cart/page.jsx
@@ -0,0 +1,7 @@
+export default function Cart() {
+  return (
+    <div className="container-fluid">
+      <h1>Carrito</h1>
+    </div>
+  );
+}
---
+++ app/context/links.jsx
@@ -0,0 +1,26 @@
+export const links = [
+  {
+    label: "Inicio",
+    route: "/main"
+  },
+  {
+    label: "Ofertas",
+    route: "/ofertas",
+  },
+  {
+    label: "Productos",
+    route: "/productos",
+  },
+  {
+    label: "Marcas",
+    route: "/marcas",
+  },
+  {
+    label: "Login",
+    route: "/login",
+  },
+  {
+    label: "Carrito",
+    route: "/cart",
+  },
+];
