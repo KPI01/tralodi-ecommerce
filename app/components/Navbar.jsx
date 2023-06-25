ffecbe2a57d6d0616ea5d8bd5813c65363dac18f
KPI01 <jorgelurd11@outlook.com>
2023-06-25 18:57:01 +0200

    Updated react icons, made one component for each one

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,6 +4,8 @@ import SearchBar from "./navbar/SearchBar";
 import LoginBtn from "./navbar/LoginBtn";
 import CartBtn from "./navbar/CartBtn";
 import CurrencySelector from "./navbar/CurrencySelector";
+import BsList from "./svg/BsList";
+import BsTagsFill from "./svg/BsTagsFill";
 
 export default function Navbar() {
   const nav_links = links.filter(links => links.route!="/main")
@@ -32,19 +34,7 @@ export default function Navbar() {
           aria-label="Toggle navigation"
         >
           <span class="text-secondary">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              fill="currentColor"
-              class="bi bi-list"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-              />
-            </svg>
+            <BsList />
           </span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -61,17 +51,7 @@ export default function Navbar() {
                   id="nav-link"
                 >
                   {label == "Ofertas" && (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18"
-                      height="18"
-                      fill="currentColor"
-                      className="bi bi-tags-fill me-2 mb-2"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2zm3.5 4a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
-                      <path d="M1.293 7.793A1 1 0 0 1 1 7.086V2a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l.043-.043-7.457-7.457z" />
-                    </svg>
+                    <BsTagsFill />
                   )}
                   {label}
                 </Link>
--- app/components/navbar/SearchBar.jsx
+++ app/components/navbar/SearchBar.jsx
@@ -1,17 +1,10 @@
+import BsSearch from "../svg/BsSearch";
+
 export default function SearchBar() {  
   return (
     <form className="d-flex align-items-center justify-content-end text-alter1 my-md-3 mx-auto" id="form-search-bar" role="search">
       <label htmlFor="search-bar">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="18"
-        height="18"
-        fill="currentColor"
-        class="bi bi-search me-2"
-        viewBox="0 0 16 16"
-      >
-        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-      </svg>
+      <BsSearch />
       </label>
       <input
         name="search-bar"
