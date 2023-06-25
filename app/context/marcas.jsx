c7256d4709087ac1c4a420b6374cabf55044e8cb
KPI01 <jorgelurd11@outlook.com>
2023-06-25 22:12:31 +0200

    Added and updated contexts files

--- app/context/marcas.jsx
+++ app/context/marcas.jsx
@@ -1,29 +1,38 @@
 export const marcas = [
     {
+        id: 1,
         label: "Nestlé Venezuela, S.A"
     },
     {
+        id: 2,
         label: "Alfonzo Rivas & Cia, C.A."
     },
     {
+        id: 3,
         label: "PEPSICO Alimentos, S.C.A"
     },
     {
+        id: 4,
         label: "Envaprimol"
     },
     {
+        id: 5,
         label: "El Tunal, C.A."
     },
     {
-        label: "Plumrose Latinoamricana, C.A."
+        id: 6,
+        label: "Plumrose Latinoamericana, C.A."
     },
     {
+        id: 7,
         label: "Gombi Ind. Alimenticias, C.A."
     },
     {
+        id: 8,
         label: "Ind. Lacteas Torondoy, C.A."
     },
     {
+        id: 9,
         label: "General de Alimentos Nisa, C.A."
     },
 ]
--- app/context/productos.jsx
+++ app/context/productos.jsx
@@ -1,47 +1,47 @@
-export const ofertas = [
+export const productos = [
     {
         id: 1,
         label: "Producto 1",
         price: 30 ,
-        percent: 10
+        discount: 10
 
     },
     {
         id: 2,
         label: "Producto 2",
         price: 7.5,
-        percent: 10
+        discount: 10
     },
     {
         id: 3,
         label: "Producto 3",
         price: 10,
-        percent: 10
+        discount: 10
     },
     {
         id: 4,
         label: "Producto 4",
         price: 20.25 ,
-        percent: 10
+        discount: 0
     },
     {
         id: 5,
         label: "Producto 5",
         price: 35,
-        percent: 10
+        discount: 10
 
     },
     {
         id: 6,
         label: "Producto 6",
         price: 40,
-        percent: 10
+        discount: 0
 
     },
     {
         id: 7,
         label: "Producto 7",
         price: 50,
-        percent: 10
+        discount: 10
     },
 ]
