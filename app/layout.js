4cfc6f7476647297510659c429b37120e7f20af7
KPI01 <jorgelurd11@outlook.com>
2023-07-05 19:44:18 +0200

    Installed and configured bootstrap-icons

--- app/layout.js
+++ app/layout.js
@@ -2,13 +2,13 @@
 import { useEffect } from "react";
 import "/styles/scss/main.scss";
 import "/styles/css/main.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 export default function RootLayout({ children }) {
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.min.js");
-
   });
 
   return (
@@ -22,15 +22,13 @@ export default function RootLayout({ children }) {
         <link rel="icon" href="/svg/favicon.svg" />
       </head>
       <body className="bg-primary text-secondary">
-            <header className="container-fluid mb-4 mb-lg-0">
-              <Navbar />
-            </header>
-            <main>
-            <div className="container">
-                {children}
-            </div>
-            </main>
-            <Footer />
+        <header className="container-fluid mb-4 mb-lg-0">
+          <Navbar />
+        </header>
+        <main>
+          <div className="container">{children}</div>
+        </main>
+        <Footer />
       </body>
     </html>
   );
