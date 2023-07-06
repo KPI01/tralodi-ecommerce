c94c095c76205aa2b731819f0904358f0b426083
KPI01 <jorgelurd11@outlook.com>
2023-07-06 16:03:14 +0200

    Created and finished NavPills component

--- app/components/visual/NavPills.jsx
+++ app/components/visual/NavPills.jsx
@@ -1,3 +1,24 @@
-export default function NavPills () {
-    return (<></>)
+"use client";
+
+import { useState } from "react";
+
+export default function NavPills({ context }) {
+
+    const [toggleState, setToggleState] = useState(1);
+
+    const toggleTab = (index) => {
+         setToggleState(index)
+    }
+
+  return (
+    <ul id="nav-pills" class="nav nav-pills">
+      {context.map(({ id, label }) => (
+        <li id="nav-pill-item" className="nav-item mx-1" onClick={() => toggleTab(id)}>
+          <a id="nav-pill-link" className={toggleState===id ? "nav-link active" : "nav-link"} uid={id}>
+            {label}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
 }
