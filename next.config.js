a9543af104e2bb4f8bafc4f0616ca00fd3c0f796
KPI01 <jorgelurd11@outlook.com>
2023-07-19 13:25:21 +0200

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
       return [
         {
           source: '/',
-          destination: '/main',
+          destination: '/home',
           permanent: true,
         },
       ]
