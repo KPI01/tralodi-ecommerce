bf875c7cd33df8747fa7d7b4a7bd58688ab9964d
KPI01 <jorgelurd11@outlook.com>
2023-08-01 18:17:59 +0200

    Actualizado layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,33 +5,34 @@ import "/styles/css/main.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { Main } from "./context/sesion";
 
 export default function RootLayout({ children }) {
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.min.js");
   });
 
-  const [currency, setCurrency] = useState("Bs.");
-
   return (
-    <html lang="es">
-      <head>
-        <title>Tralodi Distribución</title>
-        <meta
-          title="description"
-          content="Página web comercial para Inversiones Tralodi"
-        />
-        <link rel="icon" href="/svg/favicon.svg" />
-      </head>
-      <body className="bg-primary text-secondary">
-        <header className="container-fluid mb-4 mb-lg-0">
-          <Navbar setCurrency={setCurrency} currency={currency} />
-        </header>
-        <main>
-          <div className="container">{children}</div>
-        </main>
-        <Footer />
-      </body>
-    </html>
+    <Main>
+      <html lang="es">
+        <head>
+          <title>Tralodi Distribución</title>
+          <meta
+            title="description"
+            content="Página web comercial para Inversiones Tralodi"
+          />
+          <link rel="icon" href="/svg/favicon.svg" />
+        </head>
+        <body className="bg-primary text-secondary">
+          <header className="container-fluid mb-4 mb-lg-0">
+            <Navbar  />
+          </header>
+          <main>
+            <div className="container">{children}</div>
+          </main>
+          <Footer />
+        </body>
+      </html>
+    </Main>
   );
 }
