8a83a548559227f7b6ce34e78350f7a60acb81ac
Jorge Ramírez <jorgelurd11@gmail.com>
2023-09-23 21:37:06 +0200

    Eliminado link de carrito para probar deploy

--- app/components/navbar/CartBtn.jsx
+++ app/components/navbar/CartBtn.jsx
@@ -1,6 +1,6 @@
 export default function CartBtn() {
   return (
-    <a href="/cart">
+    <a href="/">
       <button
         className="btn btn-secondary text-primary ms-auto mx-lg-auto"
         id="cart-btn"
