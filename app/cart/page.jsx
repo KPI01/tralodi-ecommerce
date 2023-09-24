a77c9fc8c251a6b61d6e3572aa1f66cf61c70894
Jorge Ramírez <jorgelurd11@gmail.com>
2023-09-24 15:26:53 +0200

    Agregada funcionalidad dentro del carrito & Corregido renderizado condicional

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -17,179 +17,231 @@ export default function Cart() {
   console.log("El subtotal del carrito es: ", subTotal);
 
   // Generando el total del carrrito
-  let total = carrito
-    .reduce((total, producto) => {
-      let ivaProducto = 0;
-      if (producto.descuento < 1) {
-        ivaProducto = producto.precio * producto.cant * 0.12;
-        return (total += producto.precio * producto.cant + ivaProducto);
-      } else {
-        ivaProducto =
-          producto.precio * producto.cant -
-          producto.precio * producto.cant * (producto.descuento / 100) * 0.12;
-        return (total += producto.precio * producto.cant + ivaProducto);
-      }
-    }, 0)
-    .toFixed(2);
+  let total = carrito.reduce((total, producto) => {
+    let ivaProducto = 0;
+    if (producto.descuento === 0) {
+      ivaProducto = producto.precio * producto.cant * 0.12;
+      return (total += producto.precio * producto.cant + ivaProducto);
+    } else {
+      ivaProducto =
+        producto.precio * producto.cant -
+        producto.precio * producto.cant * (producto.descuento / 100);
+      ivaProducto *= 0.12;
+      return (total += producto.precio * producto.cant + ivaProducto);
+    }
+  }, 0);
   console.log("El total del carrito es: ", total);
 
-  // Comprobacion que el carrito tenga mas de un producto
-  if (carrito.length < 1) {
-    return (
-      <div id="container-carrito-vacio">
-        <div
-          id="alert-carrito-vacio"
-          className="alert alert-secondary p-4 mx-auto"
-        >
-          <h4 className="mx-4 fs-2">
-            El carrito está vacío. Ve a agregar productos!
-          </h4>
-        </div>
+  // ActionCreator para agregar al carrito de determinado producto
+  const addProductoToCarrito = (id) => {
+    dispatch({
+      type: "ADD_TO_CARRITO",
+      payload: { id: id, cant: 1 },
+    });
+  };
+
+  // ActionCreator para reducir del carrito determinado producto
+  const redProductoFromCarrito = (id) => {
+    dispatch({
+      type: "RED_FROM_CARRITO",
+      payload: { id: id, cant: 1 },
+    });
+  };
+
+  // ActionCreator para eliminar del carrito determinado producto
+  const delProductoFromCarrito = (id) => {
+    dispatch({
+      type: "DEL_FROM_CARRITO",
+      payload: { id: id, cant: 1 },
+    });
+  };
+
+  // Validando que los productos del carrito tengan más de una unidad y el producto esté activo
+  let carritoValid = carrito.filter(
+    (producto) => producto.cant > 0 && producto.estado === "Activo"
+  );
+  return carritoValid.length < 1 ? (
+    <div id="container-carrito-vacio">
+      <div
+        id="alert-carrito-vacio"
+        className="alert alert-secondary p-4 mx-auto"
+      >
+        <h4 className="mx-4 fs-2">
+          El carrito está vacío. <a href="/home">Ve a agregar productos!</a>
+        </h4>
       </div>
-    );
-  } else {
-    // Validando que los productos del carrito tengan más de una unidad y el producto esté activo
-    let carritoValid = carrito.filter(
-      (producto) => producto.cant > 0 && producto.estado === "Activo"
-    );
-    return (
-      <div id="container-carrito" className="container my-4">
-        <div className="row">
-          <div className="col-8">
-            <h2 className="m-0 display-6 fw-bold">Tu carrito</h2>
-            <div
-              id="container-detalles-carrito"
-              className="bg-alter1 text-primary rounded p-3 h-100"
-            >
-              <ul id="detalles-carrito" className="list-group list-group-flush">
-                {carritoValid.map((producto) => {
-                  return (
-                    <li
-                      id="detalles-producto"
-                      className="list-group-item text-primary container"
-                    >
-                      <div className="row">
-                        <div
-                          id="producto-carrito-img"
-                          className="col d-flex justify-content-center"
-                        >
-                          <img src={"/png/productos/" + producto.id + ".png"} />
-                        </div>
-                        <div className="col text-primary d-flex align-item-center">
-                          <div class="ms-2 me-auto my-auto">
-                            <div class="fw-bold">
-                              {producto.descripcion + " " + producto.medida}
-                            </div>
-                            <strong className="fs-6">Precio unit. : </strong>{" "}
-                            {monedaActiva.simbolo}
-                            {monedaActiva.simbolo === "$"
-                              ? producto.precio
-                              : producto.precio * mndLocal.valorDolar}{" "}
-                            <br />
-                            <strong>Sub-total: </strong> {monedaActiva.simbolo}
-                            {producto.precio * producto.cant}
+    </div>
+  ) : (
+    <div id="container-carrito" className="container my-4">
+      <div className="row">
+        <div className="col-8">
+          <h2 className="m-0 display-6 fw-bold">Tu carrito</h2>
+          <div
+            id="container-detalles-carrito"
+            className="bg-alter1 text-primary rounded p-3 h-100"
+          >
+            <ul id="detalles-carrito" className="list-group list-group-flush">
+              {carritoValid.map((producto) => {
+                return (
+                  <li
+                    id="detalles-producto"
+                    className="list-group-item text-primary container"
+                  >
+                    <div className="row">
+                      <div
+                        id="producto-carrito-img"
+                        className="col d-flex justify-content-center"
+                      >
+                        <img src={"/png/productos/" + producto.id + ".png"} />
+                      </div>
+                      <div className="col text-primary d-flex align-item-center">
+                        <div class="ms-2 me-auto my-auto">
+                          <div class="fw-bold">
+                            {producto.descripcion + " " + producto.medida}
                           </div>
+                          <strong className="fs-6">Precio unit. : </strong>{" "}
+                          {monedaActiva.simbolo}
+                          {monedaActiva.simbolo === "$"
+                            ? producto.precio
+                            : producto.precio * mndLocal.valorDolar}{" "}
+                          <br />
+                          <strong>Sub-total: </strong> {monedaActiva.simbolo}
+                          {monedaActiva.simbolo === "$"
+                            ? producto.precio * producto.cant
+                            : producto.precio *
+                              producto.cant *
+                              mndLocal.valorDolar}
                         </div>
-                        <div className="col text-primary d-flex align-items-center justify-content-center">
-                          <div class="ms-2 me-auto d-flex flex-column ms-auto">
-                            <div>
-                              <strong>Cantidad: </strong>
-                              {"  "}
-                              {producto.cant}
-                            </div>
+                      </div>
+                      <div className="col text-primary d-flex align-items-center justify-content-center">
+                        <div class="ms-2 me-auto d-flex flex-column w-100">
+                          <div>
+                            <strong>Cantidad: </strong>
+                            {"  "}
+                            {producto.cant}
+                          </div>
+                          <div
+                            id="carrito-productos-btns"
+                            className="d-flex justify-content-between"
+                          >
                             <div className="btn-group">
                               <button
                                 id="carrito-producto-btn"
                                 className="btn btn-secondary border-1 border-start-0 border-top-0 border-bottom-0 border-primary"
+                                onClick={() =>
+                                  redProductoFromCarrito(producto.id)
+                                }
                               >
                                 -
                               </button>
                               <button
                                 id="carrito-producto-btn"
                                 className="btn btn-secondary border-1 border-end-0 border-top-0 border-bottom-0 border-primary"
+                                onClick={() =>
+                                  addProductoToCarrito(producto.id)
+                                }
                               >
                                 +
                               </button>
                             </div>
+                            <button
+                              id="carrito-producto-btn-delete"
+                              className="btn btn-alter2 m-0 py-0 px-2"
+                              onClick={() =>
+                                delProductoFromCarrito(producto.id)
+                              }
+                            >
+                              <i
+                                id="delete-icon"
+                                className="bi bi-trash m-aut"
+                              ></i>
+                            </button>
                           </div>
                         </div>
                       </div>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
-          <div className="col-4">
-            <h2 className="m-0 display-6 fw-bold">Resumen</h2>
-            <div
-              className="bg-alter1 text-primary rounded p-3 h-100"
-              style={{ "--bs-bg-opacity": 0.9 }}
-            >
-              <h3 id="resumen-subtotal" className="fs-5">
-                <strong>Sub-total: </strong>
-                {monedaActiva.simbolo}
-                {monedaActiva.simbolo === "$"
-                  ? subTotal
-                  : subTotal * mndLocal.valorDolar}
-              </h3>
-              <hr />
-              <h4 className="m-0 fw-bold fs-6">IVA</h4>
-              <ul id="resumen-iva-lista" className="ps-0">
-                {carritoValid.map((producto) => {
-                  return (
-                    <li
-                      id="resumen-iva-producto"
-                      className="d-flex justify-content-between"
-                    >
-                      <div>
-                        {producto.descripcion} {producto.medida}
-                      </div>
-                      <div>
-                        {monedaActiva.simbolo}
-                        {producto.descuento < 1
-                          ? monedaActiva.simbolo === "$"
-                            ? producto.precio * producto.cant * 0.12
-                            : producto.precio *
+        </div>
+        <div className="col-4">
+          <h2 className="m-0 display-6 fw-bold">Resumen</h2>
+          <div
+            className="bg-alter1 text-primary rounded p-3 h-100"
+            style={{ "--bs-bg-opacity": 0.9 }}
+          >
+            <h3 id="resumen-subtotal" className="fs-5">
+              <strong>Sub-total: </strong>
+              {monedaActiva.simbolo}
+              {monedaActiva.simbolo === "$"
+                ? subTotal
+                : subTotal * mndLocal.valorDolar}
+            </h3>
+            <hr />
+            <h4 className="m-0 fw-bold fs-6">IVA 12%</h4>
+            <ul id="resumen-iva-lista" className="ps-0">
+              {carritoValid.map((producto) => {
+                return (
+                  <li
+                    id="resumen-iva-producto"
+                    className="d-flex justify-content-between"
+                  >
+                    <div>
+                      {producto.descripcion} {producto.medida}
+                    </div>
+                    <div>
+                      {monedaActiva.simbolo}
+                      {producto.descuento < 1
+                        ? monedaActiva.simbolo === "$"
+                          ? (producto.precio * producto.cant * 0.12).toFixed(2)
+                          : (
+                              producto.precio *
                               mndLocal.valorDolar *
                               producto.cant *
                               0.12
-                          : monedaActiva.simbolo === "$"
-                          ? producto.precio -
+                            ).toFixed(2)
+                        : monedaActiva.simbolo === "$"
+                        ? (
+                            producto.precio -
                             producto.precio *
                               (producto.descuento / 100) *
                               producto.cant *
                               0.12
-                          : (
-                              (producto.precio * mndLocal.valorDolar -
-                                producto.precio *
-                                  mndLocal.valorDolar *
-                                  (producto.descuento / 100)) *
-                              producto.cant *
-                              0.12
-                            ).toFixed(2)}
-                      </div>
-                    </li>
-                  );
-                })}
-              </ul>
-              <hr />
-              <h3 className="fs-4">
-                <strong>Total: </strong>
-                {monedaActiva.simbolo}
-                {monedaActiva.simbolo === "$"
-                  ? total
-                  : total * mndLocal.valorDolar}
-              </h3>
-              <div className="w-100 d-flex justify-content-center mt-3">
-                <button id="carrito-resumen-btn-comprar" className="btn btn-secondary text-primary fw-bold rounded-pill px-3 py-2">
-                  Comprar
-                </button>
-              </div>
+                          ).toFixed(2)
+                        : (
+                            (producto.precio * mndLocal.valorDolar -
+                              producto.precio *
+                                mndLocal.valorDolar *
+                                (producto.descuento / 100)) *
+                            producto.cant *
+                            0.12
+                          ).toFixed(2)}
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+            <hr />
+            <h3 className="fs-4">
+              <strong>Total: </strong>
+              {monedaActiva.simbolo}
+              {monedaActiva.simbolo === "$"
+                ? total.toFixed(2)
+                : (total * mndLocal.valorDolar).toFixed(2)}
+            </h3>
+            <div className="w-100 d-flex justify-content-center mt-3">
+              <button
+                id="carrito-resumen-btn-comprar"
+                className="btn btn-secondary text-primary fw-bold rounded-pill px-3 py-2"
+              >
+                Comprar
+              </button>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
--- app/context/sesion.jsx
+++ app/context/sesion.jsx
@@ -4,12 +4,50 @@ import { usuario } from "./usuario";
 
 // Reducer
 export const Reducer = (state, action) => {
+  let updtCarrito = [];
+
   switch (action.type) {
     case "SET_MONEDA":
       action.type = "DONE";
 
       state.monedaActiva = action.payload;
       return { ...state };
+
+    case "ADD_TO_CARRITO":
+      state.carrito.map((producto) => {
+        if (producto.id === action.payload.id) {
+          producto.cant += action.payload.cant;
+        }
+        updtCarrito.push(producto);
+        return true;
+      });
+      state.carrito = updtCarrito;
+      action.type = "DONE";
+      return { ...state };
+
+      case "RED_FROM_CARRITO":
+        state.carrito.map((producto) => {
+            if (producto.id === action.payload.id) {
+              producto.cant -= action.payload.cant;
+            }
+            updtCarrito.push(producto);
+            return true;
+          });
+          state.carrito = updtCarrito;
+          action.type = "DONE";
+          return { ...state };
+
+        case "DEL_FROM_CARRITO":
+            state.carrito.map((producto) => {
+                if (producto.id === action.payload.id) {
+                  producto.cant = 0;
+                }
+                updtCarrito.push(producto);
+                return true;
+              });
+              state.carrito = updtCarrito;
+              action.type = "DONE";
+              return { ...state };
 
     default:
       return state;
@@ -36,17 +74,17 @@ const sesion = {
       descuento: 0,
     },
     {
-        id: 39400123,
-        empresa: 2,
-        marca: "Alfonzo Rivas",
-        descripcion: "Fruty Aros",
-        presentacion: "Bolsa",
-        medida: "25 Gr",
-        categoria: "Cereales",
-        cant: 2,
-        precio: 1,
-        estado: "Activo",
-        descuento: 30
+      id: 39400123,
+      empresa: 2,
+      marca: "Alfonzo Rivas",
+      descripcion: "Fruty Aros",
+      presentacion: "Bolsa",
+      medida: "25 Gr",
+      categoria: "Cereales",
+      cant: 2,
+      precio: 1,
+      estado: "Activo",
+      descuento: 30,
     },
   ],
 };
