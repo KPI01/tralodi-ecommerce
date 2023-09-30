f5fc1398fb0ba9b5799be74a223be63b2e4dd970
Jorge Ramírez <jorgelurd11@gmail.com>
2023-09-30 18:18:48 +0200

    Realizado el carrusel con desplazamiento automático y tiempo personalizado

--- components/visual/Carousel.jsx
+++ components/visual/Carousel.jsx
@@ -7,35 +7,35 @@ export default function Carousel() {
   });
 
   return (
-    <div id="carouselExampleRide" class="carousel slide" data-bs-ride="true">
-      <div class="carousel-inner">
-        <div class="carousel-item active" data-bs-interval="4000">
-          <img src="/png/banner-main.png" class="d-block w-100" alt="Banner 1" />
+    <div id="carouselExampleRide" className="carousel slide" data-bs-ride="true">
+      <div className="carousel-inner">
+        <div className="carousel-item active" data-bs-interval="4000">
+          <img src="/png/banner-main.png" className="d-block w-100" alt="Banner 1" />
         </div>
-        <div class="carousel-item" data-bs-interval="4000">
-          <img src="..." class="d-block w-100" alt="Banner 2" />
+        <div className="carousel-item" data-bs-interval="4000">
+          <img src="..." className="d-block w-100" alt="Banner 2" />
         </div>
-        <div class="carousel-item" data-bs-interval="4000">
-          <img src="..." class="d-block w-100" alt="Banner 3" />
+        <div className="carousel-item" data-bs-interval="4000">
+          <img src="..." className="d-block w-100" alt="Banner 3" />
         </div>
       </div>
       <button
-        class="carousel-control-prev"
+        className="carousel-control-prev"
         type="button"
         data-bs-target="#carouselExampleRide"
         data-bs-slide="prev"
       >
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
       </button>
       <button
-        class="carousel-control-next"
+        className="carousel-control-next"
         type="button"
         data-bs-target="#carouselExampleRide"
         data-bs-slide="next"
       >
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
       </button>
     </div>
   );
