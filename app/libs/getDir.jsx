8796124caaec3e913704876667d018f6468d45b6
Jorge Ramírez <jorgelurd11@gmail.com>
2023-10-15 16:03:59 +0200

    Update getDir.jsx
    Prueba de funcionamiento en deploy

--- app/libs/getDir.jsx
+++ app/libs/getDir.jsx
@@ -3,6 +3,6 @@ import { headers } from 'next/headers'
 export default async function getData (action, dir) {
   const host = headers().get('host')
 
-  const response = await fetch(`http://${host}/api/public?action=${action}&relDir=${dir}`)
+  const response = await fetch(`/api/public?action=${action}&relDir=${dir}`)
   return response.json()
 }
