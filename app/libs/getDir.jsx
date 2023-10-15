e70f5c4bc43dfc350bfa9117b7ea5e9ec3f25072
Jorge Ramirez <jorgelurd11@gmail.com>
2023-10-15 16:33:21 +0200

    Updt getDir.jsx
    Probando arreglo fetching de datos en API

--- app/libs/getDir.jsx
+++ app/libs/getDir.jsx
@@ -1,5 +1,3 @@
-import { headers } from 'next/headers'
-
 export default async function getData (action, dir) {
   const host = process?.env.REACT_APP_URL
   const protocol = process?.env.NODE_ENV === 'development' ? 'http' : 'https'
