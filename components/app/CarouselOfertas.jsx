30a67e082ff9acd1ee76fe8b21b66ca6302b2129
Jorge Ramirez <jorgelurd11@gmail.com>
2023-10-15 18:33:30 +0200

    Carruseles
    Arreglos a los carruseles

--- components/app/CarouselOfertas.jsx
+++ components/app/CarouselOfertas.jsx
@@ -5,13 +5,11 @@ export default async function CarouselOfertas () {
   const imgs = []
   const dir = 'ofertas_carousel'
   const data = await (await getData(dir))
-  
   // Guardar nombres de imágenes en array
   data.map(item => {
     imgs.push(item.name)
     return true
   })
-  
   console.log('CarouselOfertas: ', imgs, typeof imgs)
   return (
     <Carousel nombre='principal' contexto={data} dir={dir} />
