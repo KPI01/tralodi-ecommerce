39732238fa2ad93149dae424815b7434befac875
Jorge Ramirez <jorgelurd11@gmail.com>
2023-10-15 18:19:56 +0200

    Linter
    Arreglos menores para build

--- app/libs/getDir.jsx
+++ app/libs/getDir.jsx
@@ -1,20 +1,18 @@
-import { GITHUB_API } from './GITHUB_API_KEY';
-
+import { GITHUB_API } from './GITHUB_API_KEY'
 
 export default async function getData (dir) {
   console.log(GITHUB_API)
-  let options = {
+  const options = {
     method: 'GET',
     headers: {
       Accept: 'application/vnd.github+json',
       Authorization: `Bearer ${GITHUB_API}`,
       'X-GitHub-Api-Version': '2022-11-28'
     }
-  };
+  }
 
   // fetch a mi propia cuenta de github para obtener las imágenes
   const res = await fetch(`https://api.github.com/repos/KPI01/web-tralodi-ecommerce/contents/public/${dir}`, options)
 
   return res.json()
-  
 }
