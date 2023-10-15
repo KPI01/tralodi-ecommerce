f40d640db913701636061a88ec94019f5b0e0f26
Jorge Ramírez <jorgelurd11@gmail.com>
2023-10-15 16:06:24 +0200

    Update getDir.jsx

--- app/libs/getDir.jsx
+++ app/libs/getDir.jsx
@@ -1,7 +1,7 @@
-//import { headers } from 'next/headers'
+// import { headers } from 'next/headers'
 
 export default async function getData (action, dir) {
-  //const host = headers().get('host')
+  // const host = headers().get('host')
 
   const response = await fetch(`/api/public?action=${action}&relDir=${dir}`)
   return response.json()
