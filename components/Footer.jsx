0055341cb93ac56e94460a4ccf7b5d2fc0fbc98a
Jorge Ramirez <jorgelurd11@gmail.com>
2023-11-05 20:28:55 +0100

    Fixes menores en footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -58,7 +58,7 @@ export default function Footer () {
         {/* <div id='footer-nosotros' className='col-2'>
           <h6 className='fw-bold'>Nosotros</h6>
         </div> */}
-        <div id='footer-marca' className='col-auto container-fluid'>
+        <div id='footer-marca' className='col-auto container-fluid me-0'>
           <div className='row'>
             <div className='col'>
               <h5
