5c0845c1dabcfa6cf66ae8b9a8b1908b866f59bd
Jorge Ramirez <jorgelurd11@gmail.com>
2023-11-09 11:46:22 +0100

    Updt package.json; ContainerTarjetas & carousel_burbujas.css

--- components/visual/tarjetas/ContainerTarjetas.jsx
+++ components/visual/tarjetas/ContainerTarjetas.jsx
@@ -4,7 +4,7 @@ import { v4 } from 'uuid'
 export default function ContainerTarjetas ({ contexto }) {
   return (
     <div className='container-fluid'>
-      <div className='row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3'>
+      <div className='row row-cols-auto g-3'>
         {contexto.map(item => {
           return (
             <div key={v4()} className='col'>
