302144f525872fca30451dead06e84b1045e555e
Jorge Ramirez <jorgelurd11@gmail.com>
2023-11-09 11:39:06 +0100

    Add useMediaQuery
    Implementado responsive de carrusel de burbujas

--- components/app/home/ProductosMarcas.jsx
+++ components/app/home/ProductosMarcas.jsx
@@ -3,19 +3,29 @@ import { useState } from 'react'
 import { marcas } from '../../../context/AppContext'
 import CarouselBurbujas from '../../visual/carousel_burbujas/CarouselBurbujas'
 import ContainerTarjetas from '../../visual/tarjetas/ContainerTarjetas'
+import useMediaQuery from '../../../hooks/useMediaQuery'
 
 export default function ProductosMarcas ({ contextoTarjetas }) {
   // Estado que permite identificar cual proveedor ha sido seleccionado
-  // Hacer que esto sea un componente
   const [marcaAct, setMarcaAct] = useState(1)
   console.log('ID marca activa:', marcaAct)
+
+  //Media queries
+  const lgDesktop = useMediaQuery('(min-width: 1200px)')
+  const desktop = useMediaQuery('(min-width: 992px)')
+  const tablet = useMediaQuery('(min-width: 768px)')
+  const smLandscape = useMediaQuery('(min-width: 576px)')
+  console.log('ldDesktop:',lgDesktop)
+  console.log('desktop:',desktop)
+  console.log('tablet:',tablet)
+  console.log('smLandscape:',smLandscape)
 
   // Crear array de marcas para el componente
   let contextoMarcas = []
   let contador = 0
   // Subdivision en arrays más pequeños
   while (contador <= marcas.length) {
-    const separador = 3
+    const separador = lgDesktop ? 5 : desktop ? 4 : tablet ? 3 : 2
     contextoMarcas.push(marcas.slice(contador, contador + separador))
     contador += separador
   }
---
+++ hooks/useMediaQuery.js
@@ -0,0 +1,24 @@
+'use client'
+import { useEffect, useState } from 'react'
+
+function useMediaQuery (query) {
+
+    const [matches, setMatches] = useState()
+    
+    useEffect(() => {
+        const media = window.matchMedia(query)
+
+        if (media.matches !== matches) setMatches(media.matches)
+        
+        const listener = () => setMatches(media.matches)
+
+        media.addListener(listener)
+        
+        return () => media.removeListener(listener)
+    
+    },[query, matches])
+
+    return matches
+}
+
+export default useMediaQuery
